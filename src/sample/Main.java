804ab8550a0f7728fc113004e651228e3abbfeb5
Amos Chepchieng <akchieng@gmail.com>
2019-08-18 10:02:06 +0300

    Added New Dashboard FXML

---
+++ src/sample/Main.java
@@ -0,0 +1,44 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+    //define your offsets here
+    private double xOffset = 0;
+    private double yOffset = 0;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("DashFX.fxml"));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+        //grab your root here
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        //move around here
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
+        Scene scene = new Scene(root);
+        //set transparent
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
